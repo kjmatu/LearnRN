0a434cdbaf13a1387f207dea96b02fdb99c29271
Kenji Matsunaga <kenji.matsunaga@me.com>
2020-07-30 23:23:10 +0900

    add stack sample

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {createStackNavigator, HeaderTitle} from '@react-navigation/stack';
+import {useNavigation, NavigationContainer} from '@react-navigation/native';
 
 const styles = StyleSheet.create({
   container: {
@@ -9,8 +11,49 @@ const styles = StyleSheet.create({
   },
 });
 
-function Index() {
-  return <View style={styles.container} />;
-}
+const Main = () => {
+  const {navigate} = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Text>Main</Text>
+      <TouchableOpacity
+        onPress={() => {
+          console.log('go to sub!!!!');
+          navigate('Sub');
+        }}>
+        <Text>go to sub!!!!</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-export default Index;
+const Sub = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Sub</Text>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Main"
+        component={Main}
+        options={{title: 'main title', headerTintColor: 'red'}}
+      />
+      <Stack.Screen name="Sub" component={Sub} />
+    </Stack.Navigator>
+  );
+};
+
+export default function () {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+}
