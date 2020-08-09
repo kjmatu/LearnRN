adb39a40af86f3a03b988e9f855811f631746733
Kenji Matsunaga <kenji.matsunaga@me.com>
2020-08-09 23:56:45 +0900

    add tab component

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import {createStackNavigator, HeaderTitle} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs/';
 import {useNavigation, NavigationContainer} from '@react-navigation/native';
 
 const styles = StyleSheet.create({
@@ -50,10 +51,37 @@ const StackNavigator = () => {
   );
 };
 
+const TabMain = () => {
+  return (
+    <View style={styles.container}>
+      <Text>TabMain</Text>
+    </View>
+  );
+};
+
+const TabSub = () => {
+  return (
+    <View style={styles.container}>
+      <Text>TabSub</Text>
+    </View>
+  );
+};
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="TabMain" component={TabMain} />
+      <Tab.Screen name="TabSub" component={TabSub} />
+    </Tab.Navigator>
+  );
+};
+
 export default function () {
   return (
-    <NavigationContainer onStateChange={(newState) => console.log(newState)}>
-      <StackNavigator />
+    <NavigationContainer>
+      <TabNavigator />
     </NavigationContainer>
   );
 }
